f59890c9fc004f2f09e216d54e1e3aa4eaa30adb
Morad Teaha <mrad1999@gmail.com>
2023-04-18 04:57:42 +0300

    encrypt Aeskeys for group

--- functions/index.js
+++ functions/index.js
@@ -48,3 +48,67 @@ exports.GenAESkey = functions.https.onCall((data, context) => {
         }
   });
 
+
+
+  exports.EncrypGroupKeys = functions.https.onCall(async (data, context) => { // this function takes the participants object and their pk and encrypt the aes key for each one of them and returns it as an object
+    functions.logger.info("Genkey logs!", {structuredData: true});
+    try{
+      const {roomAESkey,participants} =data //participants is the map object from the selected participants in creategroup and the aes key
+      const Aeskeys = {}
+       return await Promise.all(Array.from(participants.values()).map(async (user)=>{
+        const encryptedAESkey = await EncryptAESkey(user.RSApublicKey ,roomAESkey )
+        Aeskeys[user.uid] = encryptedAESkey
+      })
+      ).then(res => {
+        return Aeskeys
+      })
+
+        
+        }catch(err){
+        console.log(err)
+        return {"error":err}
+        }
+  });
+
+
+
+
+  async function EncryptAESkey(contactedUserPK,roomAESkey){//function to encrypt the AES key using RSA
+  
+    const encryptedAESkey = await new Promise((resolve,reject)=>{
+      try{
+  
+        const data = roomAESkey
+        const publicKey = contactedUserPK
+        console.log('printing the public key :')
+        console.log(publicKey)
+        const encryptedData = crypto.publicEncrypt(
+          {
+            key: publicKey,
+            padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+            oaepHash: "sha256",
+          },
+          // We convert the data string to a buffer using `Buffer.from`
+          Buffer.from(data)
+        );
+        
+        // The encrypted data is in the form of bytes, so we print it in base64 format
+        // so that it's displayed in a more readable form
+        //console.log("encypted data: ", encryptedData.toString("base64")); 
+  
+           const encryptedAESkey = encryptedData.toString("base64")// that string represent the encrypted data we save in the database
+           console.log("successfully encrypted the key")
+           
+           resolve(encryptedAESkey)
+      }catch(err){
+        console.log('encryption failed')
+        reject(err);
+      }
+  
+  
+  
+    })
+  
+    return encryptedAESkey
+  
+  }
